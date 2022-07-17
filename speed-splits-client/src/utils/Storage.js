3dcbeae89872f3772d499c1226b6e85b90130863
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-17 03:58:10 +0100

    added basic layout

--- speed-splits-client/src/utils/Storage.js
+++ speed-splits-client/src/utils/Storage.js
@@ -1,26 +1,26 @@
-import { ArgumentNullException, InvalidOperationError } from "./errors";
+import { ArgumentNullError, InvalidOperationError } from "./errors";
 
 export default class Storage {
   static AddOrUpdate(name, item, useSerializer = false, allowNulls = true) {
-    ArgumentNullException.Guard("name", name);
+    ArgumentNullError.Guard("name", name);
     if (!item && !allowNulls)
       throw new InvalidOperationError("Adding a null item is not allowed.");
     localStorage.setItem(name, useSerializer ? JSON.stringify(item) : item);
   }
 
   static Get(name, useDeserializer = false) {
-    ArgumentNullException.Guard("name", name);
+    ArgumentNullError.Guard("name", name);
     const item = localStorage.getItem(name);
     return useDeserializer && item ? JSON.parse(item) : item;
   }
 
   static Delete(name) {
-    ArgumentNullException.Guard("name", name);
+    ArgumentNullError.Guard("name", name);
     localStorage.removeItem(name);
   }
 
   static DeleteAll(obj) {
-    ArgumentNullException.Guard("obj", obj);
+    ArgumentNullError.Guard("obj", obj);
     for (const key in obj) Storage.Delete(key);
   }
 }
