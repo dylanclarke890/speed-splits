30227b2ba047b5fe643bd4abce6a6046d1cc6cd6
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-07 23:17:38 +0100

    installed latest version of dc-javascript-utils

--- speed-splits-client/src/components/Shared/TimeDisplay/TimeDisplay.jsx
+++ speed-splits-client/src/components/Shared/TimeDisplay/TimeDisplay.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { parseMilliseconds } from "dc-javascript-utils";
 import { Time } from "../../../services/utils/dateTime/time";
 
 export default function TimeDisplay({ time, small }) {
@@ -9,7 +8,6 @@ export default function TimeDisplay({ time, small }) {
   return (
     <>
       <div className={classes}>
-        {parseMilliseconds(time)}
         {Time.toString(time, "hour")}:{Time.toString(time, "min")}:
         {Time.toString(time, "sec")}
         <span className="milli">.{Time.toString(time, "ms")}</span>
